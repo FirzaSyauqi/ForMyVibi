4ad9db3e0fc67251b8970b711596d568e54b0095
Firza Alfi As Syauqi <as.syauqhi28@gmail.com>
2024-09-12 10:12:36 +0700

    Update zhavibi3d.js

--- zhavibi3d.js
+++ zhavibi3d.js
@@ -1,23 +1,27 @@
-var radius = 300; // Adjusted radius for larger circle
-var autoRotate = true; // Auto rotate or not
-var rotateSpeed = -60; // Speed: seconds/360 degrees
-var imgWidth = 60; // Adjusted width of images
-var imgHeight = 90; // Adjusted height of images
 
-// Background music - set to 'null' if you don't want music
+var radius = 240; // how big of the radius
+var autoRotate = true; // auto rotate or not
+var rotateSpeed = -60; // unit: seconds/360 degrees
+var imgWidth = 120; // width of images (unit: px)
+var imgHeight = 170; // height of images (unit: px)
+
+// Link of background music - set 'null' if you dont want to play background music
 var bgMusicURL = 'https://raw.githubusercontent.com/FirzaSyauqi/ForMyVibi/main/JustTheTwoOfUs.mp3';
-var bgMusicControls = true; // Show music controls
+var bgMusicControls = true; // Show UI music control
 
-// Animation start delay
+
+
+// ===================== start =======================
+// animation start after 1000 miliseconds
 setTimeout(init, 1000);
 
 var odrag = document.getElementById('drag-container');
 var ospin = document.getElementById('spin-container');
 var aImg = ospin.getElementsByTagName('img');
 var aVid = ospin.getElementsByTagName('video');
-var aEle = [...aImg, ...aVid]; // Combine arrays
+var aEle = [...aImg, ...aVid]; // combine 2 arrays
 
-// Set size of images
+// Size of images
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
 
@@ -35,13 +39,16 @@ function init(delayTime) {
 }
 
 function applyTranform(obj) {
+  // Constrain the angle of camera (between 0 and 180)
   if(tY > 180) tY = 180;
   if(tY < 0) tY = 0;
+
+  // Apply the angle
   obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
 }
 
 function playSpin(yes) {
-  ospin.style.animationPlayState = (yes ? 'running' : 'paused');
+  ospin.style.animationPlayState = (yes?'running':'paused');
 }
 
 var sX, sY, nX, nY, desX = 0,
@@ -49,22 +56,22 @@ var sX, sY, nX, nY, desX = 0,
     tX = 0,
     tY = 10;
 
-// Auto spin
+// auto spin
 if (autoRotate) {
   var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
   ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
-// Add background music
+// add background music
 if (bgMusicURL) {
   document.getElementById('music-container').innerHTML += `
 <audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
-<p>Your browser does not support the audio element.</p>
+<p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
 }
 
-// Event handlers
+// setup events
 document.onpointerdown = function (e) {
   clearInterval(odrag.timer);
   e = e || window.event;
@@ -91,7 +98,6 @@ document.onpointerdown = function (e) {
       tX += desX * 0.1;
       tY += desY * 0.1;
       applyTranform(odrag);
-
       playSpin(false);
       if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
         clearInterval(odrag.timer);
